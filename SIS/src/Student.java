cdd1ffd2c05ed4c833d8ac78f8311f42022fdf8f
adair.schmitt <adair.schmitt@MU-C1-18.mullen.pvt.k12.co.us>
2025-03-11 12:50:13 -0600

    finished getters and setters

--- SIS/src/Student.java
+++ SIS/src/Student.java
@@ -22,4 +22,82 @@ public Student(String f, String l, String p1, String g1, String p2, String g2, S
 	period3=p3;
 	grade3=g3;	
 }
+
+public String getFirstName()
+{
+return firstName;
+}
+
+public void setFirstName(String f)
+{
+firstName=f; 	
+}
+
+public String getLastName()
+{
+return lastName;
+}
+
+public void setLastName(String l)
+{
+lastName=l; 	
+}
+public String getPeriod1()
+{
+return period1;
+}
+
+public void setPeriod1(String p1)
+{
+period1=p1; 	
+}
+
+public String getGrade1()
+{
+return grade1;
+}
+
+public void setGrade1(String g1)
+{
+grade1=g1; 	
+}
+public String getPeriod2()
+{
+return period2;
+}
+
+public void setPeriod2(String p2)
+{
+period2=p2; 	
+}
+
+public String getGrade2()
+{
+return grade2;
+}
+
+public void setGrade2(String g2)
+{
+grade2=g2; 	
+}
+public String getPeriod3()
+{
+return period3;
+}
+
+public void setPeriod3(String p3)
+{
+period3=p3; 	
+}
+
+public String getGrade3()
+{
+return grade3;
+}
+
+public void setGrade3(String g3)
+{
+grade3=g3; 	
+}
+
 	}
