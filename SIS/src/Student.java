396f7754eaef2c42dceb1618727eb8b50ff4f5fd
adair.schmitt <adair.schmitt@MU-C1-18.mullen.pvt.k12.co.us>
2025-03-11 12:38:40 -0600

    Started project

---
+++ SIS/src/Student.java
@@ -0,0 +1,25 @@
+
+public class Student
+	{
+		private String firstName;
+		private String lastName;
+		private String period1;
+		private String grade1;
+		private String period2;
+		private String grade2;
+		private String period3;
+		private String grade3;
+	
+public Student(String f, String l, String p1, String g1, String p2, String g2, String p3, String g3 )
+{
+
+	firstName=f;
+	lastName=l;
+	period1=p1;
+	grade1=g1;
+	period2=p2;
+	grade2=g2;
+	period3=p3;
+	grade3=g3;	
+}
+	}
