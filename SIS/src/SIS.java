dec0670b89a4eb8c7d8950415cfcfd32b8f49ea2
adair.schmitt <adair.schmitt@MU-C1-18.mullen.pvt.k12.co.us>
2025-03-13 12:26:29 -0600

    added skeleton for menu and submenus

--- SIS/src/SIS.java
+++ SIS/src/SIS.java
@@ -9,6 +9,7 @@ public class SIS
 			{
 				String file1 = "StudentList.txt"; 
 				ArrayList <Student> list = new ArrayList <Student>();
+				Scanner user= new Scanner (System.in);
 
 				try
 					{
@@ -33,6 +34,78 @@ public class SIS
 					}
 
 				
+				System.out.println("What would you like to do?");
+				System.out.println("1) Add or delete student");
+				System.out.println("2) Change students grades or schedules");
+				System.out.println("3) Sort Students");
+				int choice= user.nextInt();
+				if (choice ==1)
+					{
+						System.out.println("Pick one");
+						System.out.println("1) Add a Student");
+						System.out.println("2) Delete a Student");
+						int addOrDeleteChoice=user.nextInt();
+						if (addOrDeleteChoice==1)
+							{
+								System.out.println("Link add students method");
+							}
+						else if (addOrDeleteChoice==2)
+							{
+								System.out.println("link delete students method");
+							}
+						else
+							{
+								System.out.println("Error, please try again");
+							}
+						
+					}
+				else if (choice ==2)
+					{
+						System.out.println("Pick one");
+						System.out.println("1) Change grade");
+						System.out.println("2) Change schedule");
+						int changeGradeorSchedule=user.nextInt();
+						if ( changeGradeorSchedule==1)
+							{
+								System.out.println("Link grade method");
+							}
+						else if ( changeGradeorSchedule==2)
+							{
+								System.out.println("link schedule method");
+							}
+						else
+							{
+								System.out.println("Error, please try again");
+							}
+						
+					}
+				else if (choice ==3)
+					{
+						System.out.println("Pick one");
+						System.out.println("1) Sort by last name");
+						System.out.println("2) Sort by GPA");
+						System.out.println("2) Sort by period");
+						int sortChoice=user.nextInt();
+						if ( sortChoice==1)
+							{
+								System.out.println("Link last name method");
+							}
+						else if ( sortChoice==2)
+							{
+								System.out.println("link GPA method");
+							}
+						else if ( sortChoice==3)
+							{
+								System.out.println("link period method");
+							}
+						else
+							{
+								System.out.println("Error, please try again");
+							}
+						
+					}
 			}
+		
+		
 
 	}
