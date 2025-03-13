57b96877d3e2ba39984a496ef86d14a95149c329
amani.fuqua <amani.fuqua@MU-C1-07.mullen.pvt.k12.co.us>
2025-03-13 12:03:13 -0600

    testing something

--- SIS/src/GpaCalculator.java
+++ SIS/src/GpaCalculator.java
@@ -1,5 +1,7 @@
-
+import java.io.File;
+import java.util.ArrayList; 
+import java.util.Scanner;
 public class GpaCalculator
 	{
-
+		
 	}
--- SIS/src/SIS.java
+++ SIS/src/SIS.java
@@ -9,7 +9,13 @@ public class SIS
 				String file = "StudentList.txt"; 
 				ArrayList <Student> list = new ArrayList <Student>();
 				//for loop through get grade use if else to add a certain number to gpa
-				
+				// make a seperate array list for gpa ):
+				// 
 			}
 
+		public void createGpa()
+			{
+				
+		
+			}
 	}
