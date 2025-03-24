9bb0c9c7960636c50973315c3d7e0e85e0a8b96f
amani.fuqua <amani.fuqua@MU-C1-07.mullen.pvt.k12.co.us>
2025-03-24 10:35:31 -0600

    made progress with the switchcase structure

--- SIS/src/GpaCalculator.java
+++ SIS/src/GpaCalculator.java
@@ -7,17 +7,33 @@ import java.text.DecimalFormat;
 
 public class GpaCalculator
 	{
+		
+		public static void Calc()
+		{
+			
+		for (int i = 0; i<SIS.roster.size();i++)
+			{
+				if (SIS.roster.get(i).getGrade1().equals("A")|| SIS.roster.get(i).getGrade1().equals("A-")||SIS.roster.get(i).getGrade1().equals("A+"))
+					{
+					
+					}
+				SIS.counter++;
+			}
+		}
 		public static void whatTypeOfA()
 		{
 			
-		switch(SIS.roster.get(SIS.counter).getGrade1())
-		{
+			switch(SIS.roster.get(SIS.counter).getGrade1())
+			{
+				case "A":
+				SIS.GPA+=4.00;
+				case "A+":
+					SIS.GPA+=4.00;
+				
 			
 			
 			
-			
-			
-		}
+			}
 		
 		
 		}
