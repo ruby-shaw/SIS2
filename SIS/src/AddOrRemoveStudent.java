55f5bdd7ca8310238fdbd0bb4f1b83914d89f848
adair.schmitt <adair.schmitt@MU-C1-18.mullen.pvt.k12.co.us>
2025-03-25 12:49:31 -0600

    finished add and remove students but need to alphabetize

--- SIS/src/AddOrRemoveStudent.java
+++ SIS/src/AddOrRemoveStudent.java
@@ -1,11 +1,33 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddOrRemoveStudent
-	{
 
+	{
+		static Scanner user1= new Scanner(System.in);
+		
+	public static void choose()
+	{
+		System.out.println("Pick one");
+		System.out.println("1) Add a Student");
+		System.out.println("2) Delete a Student");
+		int addOrDeleteChoice=user1.nextInt();
+		if (addOrDeleteChoice==1)
+			{
+				addStudent();
+			}
+		else if (addOrDeleteChoice==2)
+			{
+				removeStudent();
+			}
+		else 
+			{
+				System.out.println("Error, please try again");
+			}
+	}
 		public static void addStudent()
 		{
-			Scanner user1= new Scanner(System.in);
+			
 			String newFirst;
 			String newLast;
 			String newFirstP;
@@ -23,12 +45,70 @@ public class AddOrRemoveStudent
 			
 			// make an arraylist of classes? then add grade after? figure out how to be a while there are still things left in the array list
 			//So basically remove a class from the arraylist while they choose it and also assign it to the variable
-			System.out.println("1) Biology");
-			System.out.println("2) Algrebra");
-			System.out.println("3) English");
+			ArrayList <String> classes = new ArrayList <String>();
+			classes.add("Biology");
+			classes.add("Algrebra");
+			classes.add("English");
+			
+			System.out.println("1) "+ classes.get(0));
+			System.out.println("2) "+ classes.get(1));
+			System.out.println("3) "+ classes.get(2));
 			System.out.println("First Period?");
 			int choice= user1.nextInt();
+			choice--;
+			newFirstP= classes.get(choice);
+			classes.remove(choice);	
+			System.out.println("What is the grade for "+ newFirstP+ "?");
+			newFirstG= user1.next();
 			
 			
+			
+			System.out.println("Second Period?");
+			System.out.println("1) "+ classes.get(0));
+			System.out.println("2) "+ classes.get(1));
+			choice=user1.nextInt();
+			choice--; 
+			newSecP= classes.get(choice);
+			classes.remove(choice);
+			System.out.println("What is the grade for "+ newSecP+ "?");
+			newSecG= user1.next();
+			
+			newThirdP= classes.get(0); 
+			System.out.println("What is the grade for "+ newThirdP+ "?");
+			newThirdG= user1.next();
+			
+			
+			SIS.roster.add(new Student(newFirst, newLast, 0.00, newFirstP, newFirstG, newSecP, newSecG, newThirdP, newThirdG));
+			
+			int num=0;
+			for (Student k : SIS.roster)
+				{
+					num++;
+					System.out.println(num+") "+k.getFirstName()+ " "+ k.getLastName() + " " + k.getPeriod1()+ " "+ k.getGrade1()+ " " + k.getPeriod2()+ " "+ k.getGrade2()+ " " + k.getPeriod3()+ " "+ k.getGrade3());
+				}
+			SIS.menu();
+			
+		}
+		
+		public static void removeStudent()
+		{
+			int num=0;
+			for (Student k : SIS.roster)
+				{
+					num++;
+					System.out.println(num+") "+k.getFirstName()+ " "+ k.getLastName() + " " + k.getPeriod1()+ " "+ k.getGrade1()+ " " + k.getPeriod2()+ " "+ k.getGrade2()+ " " + k.getPeriod3()+ " "+ k.getGrade3());
+				}
+			System.out.println("Which student would you like to remove?");
+			int choice= user1.nextInt();
+			choice--;
+			SIS.roster.remove(choice);
+			int newNum=0;
+			System.out.println();
+			for (Student k : SIS.roster)
+				{
+					newNum++;
+					System.out.println(newNum+") "+k.getFirstName()+ " "+ k.getLastName() + " " + k.getPeriod1()+ " "+ k.getGrade1()+ " " + k.getPeriod2()+ " "+ k.getGrade2()+ " " + k.getPeriod3()+ " "+ k.getGrade3());
+				}
+			SIS.menu();
 		}
 	}
