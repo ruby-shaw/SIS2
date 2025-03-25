d58e57b6c2ff81c0569bb5bef48dd4cb98e5e491
adair.schmitt <adair.schmitt@MU-C1-18.mullen.pvt.k12.co.us>
2025-03-25 13:16:27 -0600

    made a new class and has a bunch of errors!! WHOOOO

--- SIS/src/SIS.java
+++ SIS/src/SIS.java
@@ -72,22 +72,7 @@ public class SIS
 				else if (choice ==2)
 
 					{
-						System.out.println("Pick one");
-						System.out.println("1) Change grade");
-						System.out.println("2) Change schedule");
-						int changeGradeorSchedule=user.nextInt();
-						if ( changeGradeorSchedule==1)
-							{
-								System.out.println("Link grade method");
-							}
-						else if ( changeGradeorSchedule==2)
-							{
-								System.out.println("link schedule method");
-							}
-						else
-							{
-								System.out.println("Error, please try again");
-							}
+						
 						
 					}
 
